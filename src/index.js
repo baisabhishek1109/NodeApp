822ef8ddf8c87317d70b872fe14009724e5f18ed
abhishek singh <baisabhishek64@gamil.com>
2016-09-16 00:41:51 +0530

    adding new file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-/**
- * Created by Scrabber on 9/16/2016.
- */
+var http = require('http');
+
+(function() {
+    http.createServer(function (request, response) {
+        console.log(request.url + ' ' + request.method);
+        response.end();
+    }).listen(3000, function () {
+        console.log('started listening');
+    });
+})();
